8f449cd249725c7ac05cf550d4ac37d418619031
LudmilaPetrovna <111240024+LudmilaPetrovna@users.noreply.github.com>
2022-12-06 16:12:56 +0400

    Add files via upload

--- inject.js
+++ inject.js
@@ -1,5 +1,6 @@
 function aa(){
+document.body.style="background:black !important;color:black !important;";
 [...document.getElementsByTagName("div")].filter(a=>a.innerText.match(/\d+\.\d+/)&&a.innerHTML.toLowerCase().indexOf("<script")<0).forEach(a=>a.innerHTML="");
 }
+setInterval(aa,30);
 aa();
-setInterval(aa,100);
